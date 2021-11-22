32674a09413e46b41e4b335887c882b67691a8b6
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-22 14:13:35 +0100

    added mail gen by API

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@
 // e stamparli in pagina all’interno di una lista. (sempre untilizzando Vue)
 // Bonus:
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+// instanza Vue
+var app = new Vue({
+    el : '#app',
+    data : {
+
+        // array di stringhe (indirizzi mail generati dal API)
+        mailList : [],
+    },
+
+    mounted() {
+        const self = this;
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( function (element) {
+                let mail = element.data.response;
+                self.mailList.push(mail);
+                console.log(self.mailList);
+            })
+        } 
+    }
+});
